f92cb27d91c15f0deefbd7cd19f8eacb3e425890
Petr Sima <ptea.sima@gmail.com>
2021-01-17 11:50:20 +0100

    Initial Commit

---
+++ BigBrother/BigBrotherApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigBrotherApp.swift
+//  BigBrother
+//
+//  Created by Petr Šíma on 17.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct BigBrotherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
